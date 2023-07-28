54bfc26f64bf71daa0e9ee9c4b742beb36b765a4
Zubair Khalid <zubairkhalidce@gmail.com>
2023-07-28 16:05:49 +0100

    added main layout

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import '@styles/globals.css'
+import { Metadata } from 'next'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata: Metadata = {
+  title: 'HeyPrompt',
+  description: 'Discover & Share Amazing AI Prompts',
+}
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
